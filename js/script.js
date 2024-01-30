de0633e02d0dcd673469caf301a6434383b6b73c
Gustavo Nagai <148737051+gulinagai@users.noreply.github.com>
2024-01-30 14:53:57 -0300

    accordion refatorado - alteração de function para class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js'
-import iniciarnavaccordionlist from './modules/accordion.js'
+import AccordionList from './modules/accordion.js'
 import iniciarnavtab from './modules/tabnav.js'
 import iniciarmodal from './modules/modal.js'
 import iniciartooltip from './modules/tooltip.js'
@@ -13,8 +13,9 @@ import animacaodoscroll from './modules/scroll-animacao.js'
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init()
 
+const accordion = new AccordionList('[data-anime="accordion"] dt')
+accordion.init()
 
-iniciarnavaccordionlist()
 iniciarnavtab()
 iniciarmodal()
 iniciartooltip()
