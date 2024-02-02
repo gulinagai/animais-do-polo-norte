935095d835b47a7cd46789c96376820f151fdcad
Gustavo Nagai <148737051+gulinagai@users.noreply.github.com>
2024-02-02 16:59:13 -0300

    fetch-bitcoin refatorado - alterações na function

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,13 +1,9 @@
-export default function iniciarFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((bodyJSON) => {
-      const btcPreco = document.querySelector('.btc-preco')
-      // essa conta abaixo é para calcular quanto vale 1000 reais em bitcoin
+      const btcPreco = document.querySelector(target)
+      // e ssa conta abaixo é para calcular quanto vale 1000 reais em bitcoin
       btcPreco.innerText = (1000 / bodyJSON.BRL.sell).toFixed(4)
-      // console.log(bodyJSON.BRL.sell)
-    })
-    .catch((erro) => {
-      console.log(Error(erro))
-    })
+    }).catch(erro => console.log(Error(erro)))
 }
